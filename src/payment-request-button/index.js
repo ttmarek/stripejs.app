19d3894d0928a95f1341a872e38a77fbd2fd33e7
Thomas Marek <ttmarek@outlook.com>
2020-01-31 17:23:11 -0500

    Add note to payment request button page

--- src/payment-request-button/index.js
+++ src/payment-request-button/index.js
@@ -5,43 +5,42 @@ const stripe = Stripe(publishableKey);
 const elements = stripe.elements();
 
 const paymentRequest = stripe.paymentRequest({
-  country: 'US',
-  currency: 'usd',
+  country: "US",
+  currency: "usd",
   total: {
-    label: 'Demo total',
-    amount: 1099,
+    label: "Demo total",
+    amount: 1099
   },
   requestPayerName: true,
-  requestPayerEmail: true,
+  requestPayerEmail: true
 });
 
-
-const prButton = elements.create('paymentRequestButton', {
-  paymentRequest,
+const prButton = elements.create("paymentRequestButton", {
+  paymentRequest
 });
 
 (async () => {
   // Check the availability of the Payment Request API first.
   const result = await paymentRequest.canMakePayment();
 
-  console.log('canMakePayment: ', result);
+  console.log("canMakePayment: ", result);
 
   if (result) {
-    prButton.mount('#payment-request-button');
+    prButton.mount("#payment-request-button");
   } else {
-    document.getElementById('payment-request-button').style.display = 'none';
+    document.getElementById("payment-request-button").style.display = "none";
   }
 })();
 
 const getPaymentMethod = new Promise(resolve => {
-  paymentRequest.on('paymentmethod', resolve);
-})
+  paymentRequest.on("paymentmethod", resolve);
+});
 
 const getToken = new Promise(resolve => {
-  paymentRequest.on('token', resolve);
-})
+  paymentRequest.on("token", resolve);
+});
 
-Promise.all([ getPaymentMethod, getToken ]).then(result => {
+Promise.all([getPaymentMethod, getToken]).then(result => {
   console.log(result);
-  result[0].complete('success');
-})
+  result[0].complete("success");
+});
